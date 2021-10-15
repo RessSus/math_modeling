904aeab2528d4fcb01be58e2f848a224d63929eb
RessSus <91748995+RessSus@users.noreply.github.com>
2021-10-15 15:54:49 +0000

    laba uraaa

---
+++ lab_1_task_6.py
@@ -0,0 +1,9 @@
+x = int(input())
+y = int(input())
+z = int(input())
+w = int(input())
+v = int(input())
+f = int(input())
+a = [x, y, z, w, v, f]
+aaaa = (a[0] ** 4 - a[1] ** 6 + 3 * a[2] - a[3] + (a[4] * a[5]) ** 2) ** 0.5
+print(aaaa)
