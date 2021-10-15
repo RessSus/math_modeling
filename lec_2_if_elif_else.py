23492f28c51f7201e5764caaf6e7a6be6e939382
RessSus <91748995+RessSus@users.noreply.github.com>
2021-10-15 16:40:53 +0000

    less2

---
+++ lec_2_if_elif_else.py
@@ -0,0 +1,14 @@
+'''
+if условие 1:
+  конструкция
+elif условие2:
+  констукция 2
+'''
+a = 3
+if a > 5:
+  print('hello 5')
+elif a < 2:
+  print('hello 2')
+else:
+  print('hello')
+
---
+++ lec_2_log_oper.py
@@ -0,0 +1,10 @@
+a = 3
+b = 4 
+c = 5
+
+if a > 4 and b == 2: # and - операция логическое 'И'
+  print('Good')
+elif b > 3 or c == 5: # or - логическое 'ИЛИ'
+  print('best')
+else:
+  print('bad')
