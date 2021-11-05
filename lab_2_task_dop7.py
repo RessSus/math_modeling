6b66eced757cf3e8c8b17605cb542a4c8f463220
RessSus <91748995+RessSus@users.noreply.github.com>
2021-11-05 12:11:30 +0000

    7 poluchshe

--- lab_2_task_dop7.py
+++ lab_2_task_dop7.py
@@ -1,12 +1,5 @@
+
 for i in range (1, 10):
   print(' ')
-  print(1 * i, end=' ')
-  print(2 * i, end=' ')
-  print(3 * i, end=' ')
-  print(4 * i, end=' ')
-  print(5 * i, end=' ')
-  print(6 * i, end=' ')
-  print(7 * i, end=' ')
-  print(8 * i, end=' ')
-  print(9 * i, end=' ')
-
+  for n in range (1, 10):
+    print (n * i, end=' ')
