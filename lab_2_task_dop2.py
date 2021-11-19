632c493779556e25e78dbf328be83785f63a07f6
RessSus <91748995+RessSus@users.noreply.github.com>
2021-11-19 14:09:10 +0000

    2

--- lab_2_task_dop2.py
+++ lab_2_task_dop2.py
@@ -2,10 +2,10 @@ a = int(input())
 b = int(input())
 c = int(input())
 if a + b > c and a + c > b and b + c > a:
-  if a == b or b == c or a == c:
-    print('Треугольник существует; он равнобедренный')
-  elif a == b == c:
+  if a == b == c:
     print('Треугольник существует; он равносторонний')
+  elif a == b or b == c or a == c:
+    print('Треугольник существует; он равнобедренный')
   else:
     print('Треугольник существует; он разносторонний')
 else:
