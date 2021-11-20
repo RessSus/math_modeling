3f1ccc130b4b192b3beb084894fbefbad29379ff
RessSus <91748995+RessSus@users.noreply.github.com>
2021-11-20 20:03:20 +0000

    wtf realno

--- lec_3_task_3.py
+++ lec_3_task_3.py
@@ -3,8 +3,11 @@ from lec_3_task_1 import g
 xn = int(input())
 yn = int(input())
 vn = int(input())
-x = xn + vn * t
-y = xn + vn * t - (g * t**2) / 2
-wtf = np.zeros((5, 3))
-for t in range(0, 5):
-  print(x)
+wtf = np.zeros((6, 3))
+for t in range(0, 6):
+  x = xn + vn * t
+  y = yn + vn * t - (g * t**2) / 2
+  wtf[t, 0] = t
+  wtf[t, 1] = x
+  wtf[t, 2] = y
+print(wtf)
