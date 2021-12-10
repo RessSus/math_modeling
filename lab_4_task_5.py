710b812f161ef5e5a1bb1862c6594732736c5737
RessSus <91748995+RessSus@users.noreply.github.com>
2021-12-10 16:17:40 +0000

    3

--- lab_4_task_5.py
+++ lab_4_task_5.py
@@ -3,15 +3,18 @@ import numpy as np
 def taskKR(r):
   S = (r ** 2) * np.pi
   print(S)
+  return(S)
 
 def taskKV(kv):
   S = kv ** 2
   print(S)
+  return(S)
 
 def taskTR(a, b, c):
   p = (a + b + c) / 2
   S = (p*(p - a) * (p - b) * (p - c)) ** 0.5
   print(S)
+  return(S)
 
 print('1 - круг, 2 - квадрат, 3 -треугольник')
 
@@ -28,3 +31,4 @@ elif ans == 3:
   b = int(input())
   c = int(input())
   taskTR(a, b, c)
+  
