cf15c06f18adf1b827999a8286509cf09fd554a4
RessSus <91748995+RessSus@users.noreply.github.com>
2022-01-14 14:28:39 +0000

    2

---
+++ lec_6_ukrashat.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [3, 4, 5]
+y = [40, 10, 30]
+
+plt.plot(x, y, color='g', label='luchte', marker='>', ms=5)
+
+# --- Украшательства ---
+plt.xlabel('Coord: x') # Подись оси ОХ
+plt.ylabel('Coord: y') # Подпись оси ОУ
+plt.legend() # Вызов "легенды"
+plt.title('Base') # Общая подпись графика
+plt.grid() # Подключение сетки
+plt.show()
